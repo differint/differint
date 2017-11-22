3408142b6f527b206672a6f3072b363028111912
snimpids <snimpids@gmail.com>
2017-11-22 12:34:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup
 
+def readme():
+      with open(README.rst) as f:
+            return f.read()
+
 setup(name = 'differint',
       version = '0.1',
       description = 'Collection of algorithms for numerically calculating fractional derivatives.',
+      long_description = readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+        'Intended Audience :: Scientists',
+        'Topic :: Numerical Analysis :: Fractional Calculus',
+      ],
       url = 'http://github.com/snimpids/differint',
       author = 'Matthew Adams',
       author_email = 'Matthew.Adams@ucalgary.ca',
       license = 'MIT',
       packages = ['differint'],
       zip_safe = False,
+      include_package_data = True,
       install_requires = ['numpy'])
