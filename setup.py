d59ed92c6e384faaba226ee8eaf9d985b3d2c03c
snimpids <snimpids@gmail.com>
2017-11-22 11:41:00 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'differint',
+      version = '0.1',
+      description = 'Collection of algorithms for numerically calculating fractional derivatives.',
+      url = 'http://github.com/snimpids/differint',
+      author = 'Matthew Adams',
+      author_email = 'Matthew.Adams@ucalgary.ca',
+      license = 'MIT',
+      packages = ['differint'],
+      zip_safe = False)
