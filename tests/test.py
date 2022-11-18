7d483cf21ffd6fc27bc480eabb097f3528c3de94
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2022-11-18 13:54:49 -0500

    Added new test for negative num_points

--- tests/test.py
+++ tests/test.py
@@ -81,6 +81,8 @@ class HelperTestCases(unittest.TestCase):
             checkValues(0.1, 1j, 2, 100)
         with self.assertRaises(AssertionError):
             checkValues(0.1, 1, 2j, 100)
+        with self.assertRaises(AssertionError):
+            checkValues(0.1, 0, 1, -100)
             
     """ Unit tests for gamma function. """
     
