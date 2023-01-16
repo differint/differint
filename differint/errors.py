246591b764fe38acacb2a9890af197380381a949
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2023-01-16 15:39:03 -0800

    Updated docstring

--- differint/errors.py
+++ differint/errors.py
@@ -4,13 +4,14 @@ import numpy as np
 from differint.differint.differint import *
 
 def CaputoL1Error(alpha, f_name, domain_start=0, domain_end=1, num_points=100):
-	''' Calculate a bound on the error from applying the CaputoL1 algorithm on a function.
-		inf_norm(true - calculated) <= C * inf_norm(f'') * h ** (2 - alpha)
+    ''' Calculate a bound on the error from applying the CaputoL1 algorithm on a function.
+        inf_norm(true - calculated) <= C * inf_norm(f'') * h ** (2 - alpha)
+        where C = (alpha + 1)/Gamma(1 - alpha) * k^(2 - alpha)
 
-	see Ming Li et al (2011). A numerical evaluation and regularization of Caputo fractional
-	    derivatives. Journal of Physics: Conference Series.
-	'''
-	# Flip the domain limits if they are in the wrong order.
+    see Ming Li et al (2011). A numerical evaluation and regularization of Caputo fractional
+        derivatives. Journal of Physics: Conference Series.
+    '''
+    # Flip the domain limits if they are in the wrong order.
     if domain_start > domain_end:
         domain_start, domain_end = domain_end, domain_start
     
