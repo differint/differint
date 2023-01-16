74b60f86b3ca22815c4c1969576ad9fdea74d659
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2023-01-16 15:56:18 -0800

    Removed lipschitz template

--- differint/errors.py
+++ differint/errors.py
@@ -3,8 +3,6 @@ import numpy as np
 
 from differint.differint.differint import *
 
-def find_local_lipschitz_const()
-
 def CaputoL1Error(alpha, f_name, domain_start=0, domain_end=1, num_points=100):
     ''' Calculate a bound on the error from applying the CaputoL1 algorithm on a function.
         inf_norm(true - calculated) <= C * inf_norm(f'') * h ** (2 - alpha)
