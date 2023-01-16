004ba2d4f35bd50e2c91c79f0580267dd482d660
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2023-01-16 15:54:25 -0800

    Try formula from Karniadakis et al.

--- differint/errors.py
+++ differint/errors.py
@@ -3,6 +3,8 @@ import numpy as np
 
 from differint.differint.differint import *
 
+def find_local_lipschitz_const()
+
 def CaputoL1Error(alpha, f_name, domain_start=0, domain_end=1, num_points=100):
     ''' Calculate a bound on the error from applying the CaputoL1 algorithm on a function.
         inf_norm(true - calculated) <= C * inf_norm(f'') * h ** (2 - alpha)
@@ -40,4 +42,7 @@ def RLError(alpha, f_name, domain_start=0, domain_end=1, num_points=100):
     norm_f = np.nan_to_num(norm_f, nan=0)
     #print(norm_f)
     #print((domain_end - domain_start) ** 3 / (12 * num_points ** 2))
-    return (domain_end - domain_start) ** 3 / (12 * num_points ** 2) * norm_f
+    #return (domain_end - domain_start) ** 3 / (12 * num_points ** 2) * norm_f
+    # some const "that depends only on alpha"... but what is it??
+    C_alpha = 1
+    return C_alpha * norm_f * domain_end ** alpha * step_size ** 2
