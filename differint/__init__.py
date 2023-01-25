e1371a64aa4923134ebce670cfd674be36835d88
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2023-01-25 14:40:37 -0800

    Added new function to init

--- differint/__init__.py
+++ differint/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ['isInteger', 'isPositiveInteger','checkValues', 'functionCheck', 'poch', 'Gamma', 'Beta', 
-			'MittagLeffler', 'GLcoeffs', 'GLpoint', 'GL', 'GLI', 'CRONE', 'RLcoeffs', 'RLpoint', 
+			'MittagLeffler', 'GLcoeffs', 'GLpoint', 'GLreal', 'GL', 'GLI', 'CRONE', 'RLcoeffs', 'RLpoint', 
 			'RLmatrix', 'RL', 'GLIinterpolat', 'CaputoL1point', 'CaputoL2point', 'CaputoL2Cpoint', 
 			'CaputoFromRLpoint', 'PCcoeffs', 'PCsolver']
 from .differint import *
