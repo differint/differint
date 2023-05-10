Fixed test for complex derivative of sin

tests/test.py
@@ -187,7 +187,7 @@ class TestAlgorithms(unittest.TestCase):
         # alpha = 0.9j
         calculated = RLpoint(0.9j, lambda x: np.sin(x), -100, 2, 100, zero_i_behavior='zero')
         expected = np.sin(0.9j * np.pi / 2 + np.linspace(0, 2, 100))
-        self.assertTrue(abs(calculated-expected) <= 1e-5)
+        self.assertTrue((abs(calculated-expected) <= 1e-5).all())
         #alpha = 0.5j
         calculated = RLpoint(0.5j, lambda x: np.sin(x), -100, 2, 100, zero_i_behavior='zero')
         expected = np.sin(0.5j * np.pi / 2 + np.linspace(0, 2, 100))